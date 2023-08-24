12fccabec90c150cb6244b8d6051de986336e33a
adrhmdlz <adrianalzidan35@gmail.com>
2023-08-24 17:14:07 +0700

    update: Deleted some unused code in config.php

--- config/config.php
+++ config/config.php
@@ -1,5 +1,6 @@
 <?php
 
+// Mengkoneksikan web server dengan database utama
 $koneksi = mysqli_connect('localhost', 'root', '', 'mst_server');
 
 // Mengecek apabila koneksi mysql gagal
@@ -15,9 +16,5 @@ $commandServer = mysqli_query($koneksi, "SELECT * FROM command_server");
 
 // Mengambil filter dari URL
 $filter = isset($_GET['filter']) ? $_GET['filter'] : array();
-
-// Mengecek apakah checkbox "semua" terpilih
-$semuaTerpilih = in_array('semua', $filter);
-$availableFilters = array('tipe1', 'tipe2', 'tipe3', 'tipe4', 'tipe5');
 
 ?>
