e4c8487d23ff1035e2fe83590c624131f59cfb02
adrhmdlz <adrianalzidan35@gmail.com>
2023-08-25 08:46:42 +0700

    update: Menambahkan script baru untuk index.php.

--- script/script.js
+++ script/script.js
@@ -1,21 +1,11 @@
-// $(document).ready(function() {
-//     $('input[type="checkbox"]').on('change', function() {
-//         applyFilters();
-//     });
-    
-//     function applyFilters() {
-//         var selectedFilters = $('input[type="checkbox"]:checked').map(function() {
-//             return this.value;
-//         }).get();
-        
-//         $('.card').hide(); // Sembunyikan semua konten card
-        
-//         if (selectedFilters.length === 0 || selectedFilters.includes('semua')) {
-//             $('.card').show(); // Tampilkan semua konten jika "semua" terpilih
-//         } else {
-//             selectedFilters.forEach(function(filter) {
-//                 $('.card[data-tipe="' + filter + '"]').show(); // Tampilkan konten sesuai filter yang dipilih
-//             });
-//         }
-//     }
-// });
+// Digunakan untuk mencegah dropdown tertutup ketika memilih menu "action"
+document.addEventListener('DOMContentLoaded', function () {
+    const radioInputs = document.querySelectorAll('#label-action');
+  
+    // Mencegah dropdown tertutup saat tombol radio ditekan
+    radioInputs.forEach((radioInput) => {
+        radioInput.addEventListener('click', (event) => {
+            event.stopPropagation();
+        });
+    });
+});
